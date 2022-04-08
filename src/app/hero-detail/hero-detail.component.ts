3eaeb1cf0934bc0bce66db017ee40cf8595d5b0f
Ignacio Baptista <ibaptista@grupomercel.com>
2022-04-08 16:07:10 -0300

    Avanzado tutorial Parte 6, falta desde Search by name

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -17,8 +17,7 @@ export class HeroDetailComponent implements OnInit {
         private route: ActivatedRoute,
         private heroService: HeroService,
         private location: Location
-    ) {
-    }
+    ) {}
 
     ngOnInit(): void {
         this.getHero();
@@ -30,7 +29,15 @@ export class HeroDetailComponent implements OnInit {
             .subscribe(hero => this.hero = hero);
     }
 
+    save(): void {
+        if (this.hero) {
+            this.heroService.updateHero(this.hero)
+                .subscribe(() => this.goBack());
+        }
+    }
+
     goBack(): void {
         this.location.back();
     }
+
 }

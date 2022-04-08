756269176e398b88a4f0f8d8f14a4b9bdcc62620
Ignacio Baptista <ibaptista@grupomercel.com>
2022-04-08 11:19:54 -0300

    Terminado Parte 2 Hasta hero.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {HeroesComponent} from './heroes/heroes.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+        MessagesComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
+    heroes: Hero[] = [];
+    selectedHero?: Hero;
 
-  heroes = HEROES;
-  selectedHero?: Hero;
+    constructor(private heroService: HeroService) {}
 
-  constructor() { }
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
